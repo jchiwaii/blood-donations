554a589b68fc36413bb302f76b03ef378698ecc6
Chiwai <chiwai.kiriba@gmail.com>
2025-11-08 13:40:41 +0300

    blood requests backend

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -5,3 +5,18 @@ export interface IUser {
   password: string;
   role: string;
 }
+
+export interface IBloodRequest {
+  id: number;
+  recipient_id: number;
+  title: string;
+  description: string;
+  blood_group: string;
+  units_required: number;
+  status: string;
+  urgency: string;
+  contact_phone: string;
+  contact_email: string;
+  address: string;
+  proofs_url: string[];
+}
---
+++ src/server-actions/blood-reqests.ts
@@ -0,0 +1,123 @@
+"use server";
+
+import supabase from "@/config/supabase-config";
+
+import { IBloodRequest } from "@/interfaces";
+
+export const createBloodRequest = async (payload: Partial<IBloodRequest>) => {
+  const { data, error } = await supabase
+    .from("blood_requests")
+    .insert([payload])
+    .select()
+    .single();
+
+  if (error) {
+    return {
+      success: false,
+      message: "Failed to create blood request",
+    };
+  }
+
+  return {
+    success: true,
+    message: "Blood request created successfully",
+    data,
+  };
+};
+
+export const getBloodRequests = async (id: number) => {
+  const { data, error } = await supabase
+    .from("blood_requests")
+    .select("*")
+    .eq("recipient_id", id)
+    .order("created_at", { ascending: false });
+
+  if (error) {
+    return {
+      success: false,
+      message: "Failed to retrieve blood requests",
+    };
+  }
+
+  if (data && data.length === 0) {
+    return {
+      success: false,
+      message: "No blood requests found",
+    };
+  }
+
+  return {
+    success: true,
+    data: data[0],
+    message: "Blood requests retrieved successfully",
+  };
+};
+
+export const getAllBloodRequests = async (id: number) => {
+  const { data, error } = await supabase
+    .from("blood_requests")
+    .select("*")
+    .eq("recipient_id", id)
+    .order("created_at", { ascending: false });
+
+  if (error) {
+    return {
+      success: false,
+      message: "Failed to retrieve blood requests",
+    };
+  }
+
+  if (data && data.length === 0) {
+    return {
+      success: false,
+      message: "No blood requests found",
+    };
+  }
+
+  return {
+    success: true,
+    data,
+    message: "Blood requests retrieved successfully",
+  };
+};
+
+export const updateBloodRequestStatus = async (
+  id: number,
+  payload: Partial<IBloodRequest>
+) => {
+  const { data, error } = await supabase
+    .from("blood_requests")
+    .update(payload)
+    .eq("id", id)
+    .select()
+    .single();
+
+  if (error) {
+    return {
+      success: false,
+      message: "Failed to update blood request status",
+    };
+  }
+
+  return {
+    success: true,
+    message: "Blood request status updated successfully",
+    data,
+  };
+};
+
+export const deleteBloodRequest = async (id: number) => {
+  const { error } = await supabase.from("blood_requests").delete().eq("id", id);
+
+  if (error) {
+    return {
+      success: false,
+      message: "Failed to delete blood request",
+    };
+  }
+
+  return {
+    success: true,
+    message: "Blood request deleted successfully",
+  };
+};
